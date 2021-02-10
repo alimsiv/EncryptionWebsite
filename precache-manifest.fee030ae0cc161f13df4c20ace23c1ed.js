c78fe302bb69946ffc8bc6e830cfd2d3c149662c
Ali Sivilotti <ali@sivilotti.com>
2021-02-09 19:09:25 -0500

    Updates

---
+++ precache-manifest.fee030ae0cc161f13df4c20ace23c1ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cbc68d791c5a7c462b402b37f166a0",
+    "url": "/EncryptionWebsite/index.html"
+  },
+  {
+    "revision": "6183413613c304a7f1ba",
+    "url": "/EncryptionWebsite/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b132c1f3f5e342614804",
+    "url": "/EncryptionWebsite/static/css/main.9f2c001e.chunk.css"
+  },
+  {
+    "revision": "6183413613c304a7f1ba",
+    "url": "/EncryptionWebsite/static/js/2.1a4eb749.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/EncryptionWebsite/static/js/2.1a4eb749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b132c1f3f5e342614804",
+    "url": "/EncryptionWebsite/static/js/main.bdc90a73.chunk.js"
+  },
+  {
+    "revision": "a8b4f56a5e5a46b9555e",
+    "url": "/EncryptionWebsite/static/js/runtime-main.d4754025.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/EncryptionWebsite/precache-manifest.fee030ae0cc161f13df4c20ace23c1ed.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EncryptionWebsite/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
